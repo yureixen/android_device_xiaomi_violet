0db2a1eed82441c5aa5de3d0c5aa9ac4cc8cdcb4
YUREI <281564886+yureixen@users.noreply.github.com>
2026-09-02 14:14:41 +0600

    Modify device configuration for YAAP
    Updated product name and included YAAP configurations.

--- yaap_violet.mk
+++ yaap_violet.mk
@@ -13,11 +13,12 @@ $(call inherit-product, $(SRC_TARGET_DIR)/product/full_base_telephony.mk)
 # Inherit from violet device
 $(call inherit-product, device/xiaomi/violet/device.mk)
 
-# Inherit some common LineageOS stuff.
-$(call inherit-product, vendor/lineage/config/common_full_phone.mk)
+# Inherit some common YAAP stuff.
+$(call inherit-product, vendor/yaap/config/common_full_phone.mk)
+TARGET_BOOT_ANIMATION_RES := 1080
 
 # Device identifier. This must come after all inclusions.
-PRODUCT_NAME := lineage_violet
+PRODUCT_NAME := yaap_violet
 PRODUCT_DEVICE := violet
 PRODUCT_BRAND := xiaomi
 PRODUCT_MODEL := Redmi Note 7 Pro
