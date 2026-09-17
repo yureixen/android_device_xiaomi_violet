77b8495b36ed25eb4f064b7500799e2e04eeae08
YUREI <281564886+yureixen@users.noreply.github.com>
2026-09-18 00:29:44 +0600

    violet: Remove lineage overlay from device.mk
    Removed lineage overlay from device.mk.

--- device.mk
+++ device.mk
@@ -60,8 +60,7 @@ PRODUCT_PACKAGES += \
 
 # Overlays
 DEVICE_PACKAGE_OVERLAYS += \
-    $(LOCAL_PATH)/overlay \
-    $(LOCAL_PATH)/overlay-lineage
+    $(LOCAL_PATH)/overlay
 
 # Power
 PRODUCT_COPY_FILES += \
