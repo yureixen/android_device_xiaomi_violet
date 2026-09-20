4b6314099d3547bf4866eb2e6e23ed910a8a9e3e
YUREI <281564886+yureixen@users.noreply.github.com>
2026-09-20 14:47:35 +0600

    violet: Remove boot animation resolution and accord inclusion

--- lineage_violet.mk
+++ lineage_violet.mk
@@ -15,10 +15,8 @@ $(call inherit-product, device/xiaomi/violet/device.mk)
 
 # Inherit some common Evolution X stuff.
 $(call inherit-product, vendor/lineage/config/common_full_phone.mk)
-TARGET_BOOT_ANIMATION_RES := 1080
 TARGET_ENABLE_BLUR := false
 TARGET_USES_MINI_GAPPS := true
-TARGET_INCLUDE_ACCORD := false
 
 # Device identifier. This must come after all inclusions.
 PRODUCT_NAME := lineage_violet
